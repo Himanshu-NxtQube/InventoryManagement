367725373f03d7a75a4bd3776840997d38c435e9
Himanshu <himanshu.nxtqube@gmail.com>
2025-07-31 11:21:20 +0530

    Updated exclusions and status result, if no sticker decoded/detected, no status should be visible

--- package/json_result.py
+++ package/json_result.py
@@ -37,13 +37,15 @@ def build_json_result(image_path, img_dims, rack_dict, records, mapping_info, ex
             temp_output['RACK_ID'] = rack_dict['Q3']
             temp_output['EXCLUSION'] = exclusions['left']
             if pallet_status:
-                temp_output['STATUS'] = pallet_status[0] if pallet_status[0] != 'empty' else 'partial'
+                if exclusions['left'] == "":
+                    temp_output['STATUS'] = pallet_status[0] if pallet_status[0] != 'empty' else 'partial'
         else:
             right_rack = True
             temp_output['RACK_ID'] = rack_dict['Q4']
             temp_output['EXCLUSION'] = exclusions['right']
             if pallet_status:
-                temp_output['STATUS'] = pallet_status[1] if pallet_status[1] != 'empty' else 'partial'
+                if exclusions['right'] == "":
+                    temp_output['STATUS'] = pallet_status[1] if pallet_status[1] != 'empty' else 'partial'
 
         result = next((record for record in records if record['uniqueId'] == unique_id), None)
 
@@ -72,10 +74,14 @@ def build_json_result(image_path, img_dims, rack_dict, records, mapping_info, ex
         if pallet_status:
             if exclusions['left'] == 'Empty Rack':
                 temp_output['STATUS'] = 'empty'
-            elif pallet_status[0] == 'empty':
-                temp_output['STATUS'] = 'partial'
             else:
-                temp_output['STATUS'] = pallet_status[0]
+                temp_output['STATUS'] = ""
+            # if exclusions['left'] == 'Empty Rack':
+            #     temp_output['STATUS'] = 'empty'
+            # elif pallet_status[0] == 'empty':
+            #     temp_output['STATUS'] = 'partial'
+            # else:
+            #     temp_output['STATUS'] = pallet_status[0]
         final_output.append(temp_output)
     if not right_rack:
         temp_output = output_template.copy()
@@ -84,10 +90,14 @@ def build_json_result(image_path, img_dims, rack_dict, records, mapping_info, ex
         if pallet_status:
             if exclusions['right'] == 'Empty Rack':
                 temp_output['STATUS'] = 'empty'
-            elif pallet_status[1] == 'empty':
-                temp_output['STATUS'] = 'partial'
             else:
-                temp_output['STATUS'] = pallet_status[1]
+                temp_output['STATUS'] = ""
+            # if exclusions['right'] == 'Empty Rack':
+            #     temp_output['STATUS'] = 'empty'
+            # elif pallet_status[1] == 'empty':
+            #     temp_output['STATUS'] = 'partial'
+            # else:
+            #     temp_output['STATUS'] = pallet_status[1]
         final_output.append(temp_output)
 
     return final_output
