9d9fe1368dd9f61042fcdd98a291631b0b92323a
Himanshu <himanshu.nxtqube@gmail.com>
2025-08-21 15:37:17 +0530

    fixed bugs in mahindra infer func

--- package/infer functions/mahindra_infer_func.py
+++ package/infer functions/mahindra_infer_func.py
@@ -12,10 +12,19 @@ def infer_Q3_Q4(rack_dict: dict) -> dict:
     if 'Q3' in rack_dict.keys() and 'Q4' in rack_dict.keys():
         if rack_dict['Q3'][:-2] != rack_dict['Q4'][:-2] or abs(int(rack_dict['Q3'][-2:]) - int(rack_dict['Q4'][-2:])) != 1:
 
-                if 'Q1' in rack_dict.keys() and rack_dict['Q4'] == rack_dict['Q1'][:-4] + chr(ord(rack_dict['Q1'][-4]) - 1) + rack_dict['Q1'][-3:]:
-                    rack_dict.pop('Q3', None)
-                elif 'Q2' in rack_dict.keys() and rack_dict['Q3'] == rack_dict['Q2'][:-4] + chr(ord(rack_dict['Q2'][-4]) - 1) + rack_dict['Q2'][-3:]:
-                    rack_dict.pop('Q4', None)
+                if 'Q1' in rack_dict.keys():
+                    if rack_dict['Q4'] == rack_dict['Q1'][:-4] + chr(ord(rack_dict['Q1'][-4]) - 1) + rack_dict['Q1'][-3:]:
+                        rack_dict.pop('Q3', None)
+                    elif rack_dict['Q3'][:-3] == rack_dict['Q1'][:-4] + chr(ord(rack_dict['Q1'][-4]) - 1):
+                        if abs(int(rack_dict['Q3'][-2:]) - int(rack_dict['Q1'][-2:])) == 1:
+                            rack_dict.pop('Q4', None)
+
+                elif 'Q2' in rack_dict.keys():
+                    if rack_dict['Q3'] == rack_dict['Q2'][:-4] + chr(ord(rack_dict['Q2'][-4]) - 1) + rack_dict['Q2'][-3:]:
+                        rack_dict.pop('Q4', None)
+                    elif rack_dict['Q4'][:-3] == rack_dict['Q2'][:-4] + chr(ord(rack_dict['Q2'][-4]) - 1):
+                        if abs(int(rack_dict['Q4'][-2:]) - int(rack_dict['Q2'][-2:])) == 1:
+                            rack_dict.pop('Q3', None)
                 else:
                     return {}
 
@@ -77,4 +86,4 @@ def infer_Q3_Q4(rack_dict: dict) -> dict:
 
 # === EXAMPLE ===
 if __name__ == '__main__':
-    print(infer_Q3_Q4({'HD-07/D/08': 'Q1'}))
+    print(infer_Q3_Q4({'Q2': 'HD-04/C/02','Q1': 'HD-04/C/01'}))
